7c9460994ff589fe4a5c2573e65f82313ce8e586
ItzKuroYT <isaaccooper926@gmail.com>
2026-06-10 00:59:50 -0400

    Update config.js

--- config.js
+++ config.js
@@ -20,14 +20,14 @@ const IconRealmsConfig = {
     theme: "dark"
   },
   nav: [
-    ["Home", "/home"],
+    ["Home", "/home/"],
     ["News", "/news/"],
-    ["Forums", "/forums"],
-    ["Gamemodes", "/gamemodes"],
-    ["Community", "/community"],
-    ["Staff", "/staff"],
-    ["Supporters", "/supporters"],
-    ["Store", "/store"]
+    ["Forums", "/forums/"],
+    ["Gamemodes", "/gamemodes/"],
+    ["Community", "/community/"],
+    ["Staff", "/staff/"],
+    ["Supporters", "/supporters/"],
+    ["Store", "/store/"]
   ],
   home: {
     kicker: "Landing pad",
